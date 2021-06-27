1bc3281c0f306c71fd3eb6ad290ee88a732169a5
emptyloominaty <cipuksiblol@gmail.com>
2021-06-27 20:24:46 +0200

    changed Memory config to range

--- js/configHTMLOnly.js
+++ js/configHTMLOnly.js
@@ -7,18 +7,21 @@ let config = {
         if (value<512) {value=512}
         if (value>65536) {value=65536}
         memorySize =  value
+        document.getElementById("inputConfigCpuMemory_val").innerText = value/1024
     },
     setVideoRam: function () {
         let value = (+document.getElementById("inputConfigVideoMemory").value)*1024
         if (value<8192) {value=8192}
         if (value>65536) {value=65536}
         vramSize = value
+        document.getElementById("inputConfigVideoMemory_val").innerText = value/1024
     },
     setCharMemory: function () {
         let value = (+document.getElementById("inputConfigCharMemory").value)*1024
         if (value<1024) {value=1024}
         if (value>65536) {value=65536}
-        vramSize =  value
+        charRomSize =  value
+        document.getElementById("inputConfigCharMemory_val").innerText = value/1024
     },
     setUserStorage: function () {
         let value = (+document.getElementById("inputConfigUserStorage").value)*1024
@@ -26,12 +29,14 @@ let config = {
         if (value>65536) {value=65536}
         userStorageSize =  value
         document.getElementById("capacityUserStorage").innerHTML = "Capacity: "+value/1024+"kB"
+        document.getElementById("inputConfigUserMemory_val").innerText = value/1024
     },
     setExtRam: function () {
         let value = (+document.getElementById("inputConfigExtRam").value)*1024
         if (value<0) {value=0}
         if (value>327680) {value=327680}
         extMemorySize =  value
+        document.getElementById("inputConfigExtMemory_val").innerText = value/1024
     }
 }
 
